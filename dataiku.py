ae36de2507315cb3f7fa193bd587095d6bbfeb46
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2018-10-10 19:07:21 +0200

    reachedInTime now takes into account the countdown instead of a fixed value

--- dataiku.py
+++ dataiku.py
@@ -4,23 +4,24 @@ class C3PO:
 	def __init__(self, milleniumFalconJsonFile):
 		self.milleniumFalconJsonFile = milleniumFalconJsonFile
 
-	def reachedInTime(milleniumFalconJson):
+	def reachedInTime(milleniumFalconJson, empireJson):
 		total = 0
 		autonomy = milleniumFalconJson['autonomy']
+		countdown = empireJson['countdown']
 		for routes in milleniumFalconJson['routes']:
 			# add the travel times until it reaches the first instance of Endor
 			total += routes['travelTime']
 			if total % autonomy == 0:
 				total += 1
-			if (routes['destination'] == "Endor" and total > 6):
+			if (routes['destination'] == "Endor" and total > countdown):
 				return False
 		return True
 
-	def giveMeTheOdds(milleniumFalconJsonFile, empireJsonFile): 
+	def giveMeTheOdds(milleniumFalconJsonFile, empireJsonFile):
 		milleniumFalconJson = json.load(open(milleniumFalconJsonFile))
 		empireJson = json.load(open(empireJsonFile))
 		# parse the travelTime from start to finish
-		if (C3PO.reachedInTime(milleniumFalconJson)):
+		if (C3PO.reachedInTime(milleniumFalconJson, empireJson)):
 			pass
 		return 0
 
