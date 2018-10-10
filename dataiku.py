eb03966ce95034326ff4db7ee4a765bac6dbd776
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2018-10-10 18:29:54 +0200

    Parse everything and calculate if the millenium reached Endor in time

---
+++ dataiku.py
@@ -0,0 +1,26 @@
+import json
+
+class C3PO:
+	def __init__(self, milleniumFalconJsonFile):
+		self.milleniumFalconJsonFile = milleniumFalconJsonFile
+
+	def giveMeTheOdds(milleniumFalconJsonFile, empireJsonFile): 
+		milleniumFalconJson = json.load(open(milleniumFalconJsonFile))
+		empireJson = json.load(open(empireJsonFile))
+		# parse the travelTime from start to finish
+		total = 0
+		autonomy = milleniumFalconJson['autonomy']
+		for routes in milleniumFalconJson['routes']:
+			# add the travel times until it reaches the first instance of Endor
+			total += routes['travelTime']
+			if total % autonomy == 0:
+				total += 1
+			if (routes['destination'] == "Endor" and total > 6):
+				return 0
+		return 1
+
+# I feel like I should be using json.load here instead. 
+# actually no this doesn't matter.
+milleniumFalconJsonFile = "./millenium-falcon.json"
+empireJsonFile = "./empire.json"
+print(C3PO.giveMeTheOdds(milleniumFalconJsonFile, empireJsonFile))
