c25a2e187de957c83295670cfdf8cb52a1112df1
sadamant <sadamant@student.42.us.org>
2018-10-14 20:43:13 +0200

    Add minimum amount of time taken to travel for a route

--- dataiku.py
+++ dataiku.py
@@ -12,18 +12,36 @@ class C3PO:
 		paths = [['Tatooine']]
 		# iterate through each new destination and add it to an origin 
 		for route in self.milleniumFalconJson['routes']:
-			print('the current route is ' + route['origin'] + ' to ' + route['destination'])
 			for path in paths:
 				if route['origin'] in path:
 					if path[-1] == route['origin']:
+						path.append(route['travelTime'])
 						path.append(route['destination'])
 					else:
 						indexOf = path.index(route['origin'])
 						toAppend = path[:indexOf + 1]
+						toAppend.append(route['travelTime'])
 						toAppend.append(route['destination'])
 						paths.append(toAppend)
 						break
-			print(paths)
+		return paths
+
+	# Calculate the minimum amount of travel time taken for a route
+	def calculateMinimumTime(self, paths):
+		path_times = []
+		for path in paths:
+			total = 0
+			for route in path:
+				if "Endor" not in path:
+					break
+				if route == "Endor":
+					break
+				if isinstance(route, int):
+					total += route
+				if total % self.autonomy == 0:
+					total += 1
+			path_times.append(total)
+		return path_times
 
 	# Check if the Millenium can reach Endor before the Death Star
 	# annilihates it. 
@@ -47,7 +65,8 @@ class C3PO:
 		return (sumProbability)
 
 	def giveMeTheOdds(self, empireJsonFile):
-		C3PO.generatePaths(self)
+		paths = C3PO.generatePaths(self)
+		path_times = C3PO.calculateMinimumTime(self, paths)
 		empireJson = jsoncfg.load(empireJsonFile)
 		# parse the travelTime from start to finish
 		if (C3PO.reachedInTime(self, empireJson)):
