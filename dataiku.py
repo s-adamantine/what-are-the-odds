05f06c30be8ded2d1df1c30a54149dda62ea74e7
sadamant <sadamant@student.42.us.org>
2018-10-14 21:41:15 +0200

    Create an Empire class

--- dataiku.py
+++ dataiku.py
@@ -1,6 +1,22 @@
 import jsoncfg
 from copy import deepcopy
 
+class Empire:
+	def __init__(self, empireJsonFile):
+		self.json = jsoncfg.load(empireJsonFile)
+		self.paths = Empire.grabEmpirePath(self.json)
+		self.countdown = self.json['countdown']
+
+	# Convert empireJsonFile into an easier to read data structure
+	def grabEmpirePath(empireJson):
+		empire_path = {}
+		for path in empireJson['bounty_hunters']:
+			if path['planet'] not in empire_path:
+				empire_path[path['planet']] = [path['day']]
+			else:
+				empire_path[path['planet']].append(path['day'])
+		return empire_path
+
 class C3PO:
 	def __init__(self, milleniumFalconJsonFile):
 		self.milleniumFalconJson = jsoncfg.load(milleniumFalconJsonFile)
@@ -67,9 +83,9 @@ class C3PO:
 	def giveMeTheOdds(self, empireJsonFile):
 		paths = C3PO.generatePaths(self)
 		path_times = C3PO.calculateMinimumTime(self, paths)
-		empireJson = jsoncfg.load(empireJsonFile)
+		empire = Empire(empireJsonFile)
 		# parse the travelTime from start to finish
-		if (C3PO.reachedInTime(self, empireJson)):
+		if (C3PO.reachedInTime(self, empire.json)):
 			return 1
 		return 0
 
